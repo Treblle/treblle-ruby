ef6dac365dc500de8cfc9c1c96f789cafccd91b2
Hrvoje <hrvoje.grdic1@gmail.com>
2021-08-25 15:26:57 +0200

    impl sending

--- lib/treblle.rb
+++ lib/treblle.rb
@@ -1,126 +1,82 @@
+require 'uri'
+require 'net/http'
+require 'treblle/data_builder'
+
 class Treblle
-  def initialize app
+  TREBLLE_URI = 'https://rocknrolla.treblle.com'.freeze
+
+  def initialize(app)
     @app = app
   end
 
   def call(env)
-    request = ActionDispatch::Request.new env
-    started_on = Time.now
+    request = ActionDispatch::Request.new(env)
+    started_at = Time.now
+
     begin
       status, headers, response = @app.call(env)
-      puts '*' * 100
       to_parse = response.clone
       to_parse = to_parse.try(:body) if to_parse.respond_to?(:body)
 
-      # return [status, headers, response] unless to_parse.respond_to?(:reduce)
-      # string_response = to_parse.reduce('') { |str, res| res += str }
       begin
         json_response = JSON.parse(to_parse)
-      rescue JSON::ParserError
+      rescue JSON::ParserError, TypeError
         return [status, headers, response]
       end
-      # puts json_response
-      log(env, status, started_on, Time.now, request, headers, json_response)
+
+      params = {
+        env: env,
+        status: status,
+        started_at: started_at,
+        ended_at: Time.now,
+        request: request,
+        headers: headers,
+        json_response: json_response
+      }
+      capture(params)
     rescue Exception => exception
       puts 'IN EXCEPTION'
-      status = determine_status_code_from_exception(exception)
-      log(env, status, started_on, Time.now, request, headers, nil, exception)
+      status = status_code_for_exception(exception)
+      params = {
+        env: env,
+        status: status,
+        started_at: started_at,
+        ended_at: Time.now,
+        request: request,
+        headers: headers,
+        exception: exception
+      }
+      capture(params)
       raise exception
     end
 
     [status, headers, response]
   end
 
-  def log(env, status, started_on, ended_on, request, headers, json_response, exception = nil)
-    url = env['REQUEST_URI']
-    path = env['PATH_INFO']
-    user = try_current_user(env)
-    time_spent = ended_on - started_on
-    user_agent = env['HTTP_USER_AGENT']
-    ip = env['action_dispatch.remote_ip'].calculate_ip
-    request_method = env['REQUEST_METHOD']
-    http_host = env['HTTP_HOST']
-    api_key = ENV.fetch('TREBLLE_API_KEY') { '' }
-    project_id = ENV.fetch('TREBLLE_PROJECT_ID') { '' }
-    version = '0.6'
-    sdk = 'ruby'
+  def capture(params)
+    data = DataBuilder.new(params).call
 
-    data = {
-      server: {
-        ip: '',
-        timezone: Time.zone.name,
-        software: '',
-        signature: '',
-        protocol: '',
-        os: {
-        }
-      },
-      language: {
-        name: 'ruby',
-        version: RUBY_VERSION
-      },
-      request: {
-        timestamp: started_on.to_formatted_s(:db),
-        ip: ip,
-        url: url,
-        user_agent: user_agent,
-        method: request_method,
-        headers: headers,
-        body: request.request_parameters
-      },
-      response: {
-        headers: '',
-        code: status,
-        size: json_response&.size,
-        load_time: time_spent,
-        body: json_response,
-        errors: [
-          {
-            source: exception && 'onError',
-            type: exception&.class,
-            message: exception&.message,
-            file: '',
-            line: ''
-          }
-        ]
-      }
-    }
-
-    puts api_key
-    puts project_id
-    puts version
-    puts sdk
-
-
-    # puts status
-    # puts ip
-    # puts request_method
-    # puts url
-    # puts path
-    # puts user
-    # puts time_spent
-    # puts user_agent
-    # puts http_host
-    # puts exception&.class
-    # puts exception&.message
-    # puts json_response
-    # puts '%' * 100
-    puts data.to_json
+    send_to_treblle(data)
   rescue Exception => exception
     Rails.logger.error(exception.message)
   end
 
-  def determine_status_code_from_exception(exception)
+  def send_to_treblle(data)
+    uri = URI(TREBLLE_URI)
+    req = Net::HTTP::Post.new(uri)
+    req['Content-Type'] = 'application/json'
+    req['x-api-key'] = ENV.fetch('TREBLLE_API_KEY') { '' }
+    req.body = data.to_json
+    res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => true) do |http|
+      http.request(req)
+    end
+    puts res.try(:body)
+  end
+
+  def status_code_for_exception(exception)
     exception_wrapper = ActionDispatch::ExceptionWrapper.new(nil, exception)
     exception_wrapper.status_code
   rescue
     500
-  end
-
-  def try_current_user(env)
-    controller = env['action_controller.instance']
-    return unless controller.respond_to?(:current_user, true)
-    return unless [-1, 0].include?(controller.method(:current_user).arity)
-    controller.__send__(:current_user)
   end
 end
---
+++ lib/treblle/data_builder.rb
@@ -0,0 +1,133 @@
+require 'socket'
+
+class DataBuilder
+  DEFAULT_SENSITIVE_FIELDS = [
+    "password",
+    "pwd",
+    "secret",
+    "password_confirmation",
+    "passwordConfirmation",
+    "cc",
+    "card_number",
+    "cardNumber",
+    "ccv",
+    "ssn",
+    "credit_score",
+    "creditScore",
+  ].freeze
+
+  attr_accessor :env, :status, :started_at, :ended_at, :request, :headers, :json_response, :exception
+
+  def initialize(params)
+    @env = params[:env]
+    @status = params[:status]
+    @started_at = params[:started_at]
+    @ended_at = params[:ended_at]
+    @request = params[:request]
+    @headers = params[:headers]
+    @json_response = params[:json_response]
+    @exception = params[:exception]
+  end
+
+  def call
+    url = env['REQUEST_URI']
+    path = env['PATH_INFO']
+    user = try_current_user(env)
+    time_spent = ended_at - started_at
+    user_agent = env['HTTP_USER_AGENT']
+    ip = env['action_dispatch.remote_ip'].calculate_ip
+    request_method = env['REQUEST_METHOD']
+    http_host = env['HTTP_HOST']
+    project_id = ENV.fetch('TREBLLE_PROJECT_ID') { '' }
+    version = '0.6'
+    sdk = 'ruby'
+
+    data = {
+      api_key: ENV.fetch('TREBLLE_API_KEY') { '' },
+      project_id: project_id,
+      version: version,
+      sdk: sdk,
+      data: {
+        server: {
+          ip: server_ip,
+          timezone: Time.zone.name,
+          software: '',
+          signature: '',
+          protocol: '',
+          os: {
+          }
+        },
+        language: {
+          name: 'ruby',
+          version: RUBY_VERSION
+        },
+        request: {
+          timestamp: started_at.to_formatted_s(:db),
+          ip: ip,
+          url: url,
+          user_agent: user_agent,
+          method: request_method,
+          headers: request.headers.env.reject { |key| key.to_s.include?('.') },
+          body: request.request_parameters
+        },
+        response: {
+          headers: headers,
+          code: status,
+          size: json_response&.size,
+          load_time: time_spent,
+          body: json_response,
+          errors: build_error_object(exception)
+        }
+      }
+    }
+
+    # puts api_key
+    # puts project_id
+    # puts version
+    # puts sdk
+
+
+    # puts status
+    # puts ip
+    # puts request_method
+    # puts url
+    # puts path
+    # puts user
+    # puts time_spent
+    # puts user_agent
+    # puts http_host
+    # puts exception&.class
+    # puts exception&.message
+    # puts json_response
+    # puts '%' * 100
+    # puts data.to_json
+    return data.to_json
+  end
+
+  private
+
+  def build_error_object(exception)
+    return [] unless exception.present?
+
+    [
+      {
+        source: 'onError',
+        type: exception.class,
+        message: exception.message,
+        file: '',
+        line: ''
+      }
+    ]
+  end
+
+  def try_current_user(env)
+    controller = env['action_controller.instance']
+    return unless controller.respond_to?(:current_user, true)
+    return unless [-1, 0].include?(controller.method(:current_user).arity)
+    controller.__send__(:current_user)
+  end
+
+  def server_ip
+    Socket.ip_address_list.find { |ai| ai.ipv4? && !ai.ipv4_loopback? }.ip_address
+  end
+end
