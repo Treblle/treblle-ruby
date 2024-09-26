a795f22129b19ef0474feace17d13c9adf7a340a
Borna Kapusta <borna@rubylabs.co>
2024-09-26 18:03:41 +0200

    fix calling super with arguments

--- lib/treblle/errors/configuration_errors.rb
+++ lib/treblle/errors/configuration_errors.rb
@@ -2,19 +2,19 @@ module Treblle
   module Errors
     class ConfigurationError < StandardError
       def initialize(msg = "Configuration error")
-        super(msg)
+        super
       end
     end
 
     class MissingApiKeyError < ConfigurationError
       def initialize(msg = "API key is missing")
-        super(msg)
+        super
       end
     end
 
     class MissingProjectIdError < ConfigurationError
       def initialize(msg = "Project ID is missing")
-        super(msg)
+        super
       end
     end
   end
